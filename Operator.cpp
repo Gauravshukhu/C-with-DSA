176c2351870b9436cabd46bdf92710a22253922e
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2024-02-08 20:10:33 +0530

    Add files via upload

---
+++ Operator.cpp
@@ -0,0 +1,14 @@
+/* #include <iostream>
+using namespace std;
+int main() {
+    int a = 2.0/5;
+    cout << a << endl; 
+    cout << 2.0/5 << endl;
+}*/
+#include <cmath>
+#include <iostream>
+using namespace std ;
+int main(){
+    cout << sqrt(10) <<endl;
+    cout << log(2) <<endl;
+}
