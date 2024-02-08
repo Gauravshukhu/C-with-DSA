82f31378672747410fa3a88921b3df61b5d885c8
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2024-02-08 15:05:25 +0530

    Add files via upload

---
+++ DataType and Variable.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+   /* int a = 456;
+    cout << a << endl; 
+
+    int b = 76;
+     cout << b << endl;
+
+     bool bl = false ;
+     cout << bl << endl;
+
+     float fl = 4.756789;
+     cout << fl << endl;
+
+     double db  = 5.78;
+     cout << db << endl;
+
+     int size = sizeof(a);
+     cout <<  size << endl;*/
+
+     int b = 'b';
+     cout << b << endl;
+    
+
+
+}
