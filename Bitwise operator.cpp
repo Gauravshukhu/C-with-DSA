119c8e6bdb8604cccfbd048c1dfa16a59251c076
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2024-02-12 22:51:44 +0530

    Add files via upload

---
+++ Bitwise operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    int a = 2;
+    int b =3;
+    
+
+   cout<< " a&b is " << (a&b) <<endl; //and operator (&)
+    cout<< " a|b is " << (a|b) <<endl;//or operator (|)
+     cout<< " ~b is " << ~b <<endl;//nor operator (~)
+       cout<< " a^b is" << (a^b) <<endl; // XOR operator (^)
+
+       cout << (17>>1) << endl;  // right operator
+       cout << (17>>2) << endl;  // right operator
+       cout << (19<<1) << endl;  // left operator
+
+    int i = 7;
+     cout<<(i++)<<endl; // post increment 7 , i=8
+     cout<<(++i)<<endl; // pre increment i=9
+     cout<<(i--)<<endl; // post Decrement 9 , i=8
+     cout<<(--i)<<endl; // pre Decrement 7
+
+}
